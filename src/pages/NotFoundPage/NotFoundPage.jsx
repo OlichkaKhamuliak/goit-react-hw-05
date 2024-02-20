9d18ecb604f8154ccabff1ad30e5de6c3c610325
OlichkaKhamuliak <olkoboyko97@gmail.com>
2024-02-20 14:46:05 +0200

    edit 404 styles

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { GoBackBtn } from '../../components/GoBackBtn/GoBackBtn';
 import css from './NotFoundPage.module.css';
-import gif from '../../assets/404-page-design-animation.gif';
+// import gif from '../../assets/404-page-design-animation.gif';
+import img from '../../assets/404Error.jpg';
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
@@ -41,7 +42,8 @@ const NotFoundPage = () => {
         <p className={css.seconds}>{secondsLeft}</p>
         <p className={css.text}>seconds...</p>
       </div>
-      <img src={gif} className={css.gif} alt="404 page design animation gif" />
+      {/* <img src={gif} className={css.gif} alt="404 page design animation gif" /> */}
+      <img src={img} className={css.gif} alt="404 page img" />
     </div>
   );
 };
